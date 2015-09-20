8c128382ac31e6f4c4c56a024a5185c6a718ee50
NestorMunguia <nestor_13@unitec.edu>
2015-09-20 00:03:26 -0600

    formulario nuevo

---
+++ ProyectoPlaneacion/ProyectoPlaneacion/Factibilidad.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace ProyectoPlaneacion
+{
+    partial class Factibilidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblProyecto = new System.Windows.Forms.Label();
+            this.lblAreaS = new System.Windows.Forms.Label();
+            this.lblAreaA = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblDuracion = new System.Windows.Forms.Label();
+            this.lblRecursos = new System.Windows.Forms.Label();
+            this.lblBeneficios = new System.Windows.Forms.Label();
+            this.lblTecnica = new System.Windows.Forms.Label();
+            this.lblEconomica = new System.Windows.Forms.Label();
+            this.lblOperacional = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Proyecto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Area Solicitante";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Areas Afectadas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descripcion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(37, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Duracion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(37, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Recursos";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(37, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Beneficios";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(37, 307);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Tecnica";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(37, 349);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Economica";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(37, 388);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Operacional";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(149, 265);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(260, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Estudio de Factibilidad ";
+            // 
+            // lblProyecto
+            // 
+            this.lblProyecto.AutoSize = true;
+            this.lblProyecto.Location = new System.Drawing.Point(178, 34);
+            this.lblProyecto.Name = "lblProyecto";
+            this.lblProyecto.Size = new System.Drawing.Size(10, 13);
+            this.lblProyecto.TabIndex = 11;
+            this.lblProyecto.Text = ".";
+            // 
+            // lblAreaS
+            // 
+            this.lblAreaS.AutoSize = true;
+            this.lblAreaS.Location = new System.Drawing.Point(178, 69);
+            this.lblAreaS.Name = "lblAreaS";
+            this.lblAreaS.Size = new System.Drawing.Size(13, 13);
+            this.lblAreaS.TabIndex = 12;
+            this.lblAreaS.Text = "..";
+            // 
+            // lblAreaA
+            // 
+            this.lblAreaA.AutoSize = true;
+            this.lblAreaA.Location = new System.Drawing.Point(178, 98);
+            this.lblAreaA.Name = "lblAreaA";
+            this.lblAreaA.Size = new System.Drawing.Size(16, 13);
+            this.lblAreaA.TabIndex = 13;
+            this.lblAreaA.Text = "...";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(178, 131);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(19, 13);
+            this.lblDescripcion.TabIndex = 14;
+            this.lblDescripcion.Text = "....";
+            // 
+            // lblDuracion
+            // 
+            this.lblDuracion.AutoSize = true;
+            this.lblDuracion.Location = new System.Drawing.Point(178, 157);
+            this.lblDuracion.Name = "lblDuracion";
+            this.lblDuracion.Size = new System.Drawing.Size(22, 13);
+            this.lblDuracion.TabIndex = 15;
+            this.lblDuracion.Text = ".....";
+            // 
+            // lblRecursos
+            // 
+            this.lblRecursos.AutoSize = true;
+            this.lblRecursos.Location = new System.Drawing.Point(178, 188);
+            this.lblRecursos.Name = "lblRecursos";
+            this.lblRecursos.Size = new System.Drawing.Size(25, 13);
+            this.lblRecursos.TabIndex = 16;
+            this.lblRecursos.Text = "......";
+            // 
+            // lblBeneficios
+            // 
+            this.lblBeneficios.AutoSize = true;
+            this.lblBeneficios.Location = new System.Drawing.Point(178, 220);
+            this.lblBeneficios.Name = "lblBeneficios";
+            this.lblBeneficios.Size = new System.Drawing.Size(28, 13);
+            this.lblBeneficios.TabIndex = 17;
+            this.lblBeneficios.Text = ".......";
+            // 
+            // lblTecnica
+            // 
+            this.lblTecnica.AutoSize = true;
+            this.lblTecnica.Location = new System.Drawing.Point(178, 310);
+            this.lblTecnica.Name = "lblTecnica";
+            this.lblTecnica.Size = new System.Drawing.Size(31, 13);
+            this.lblTecnica.TabIndex = 18;
+            this.lblTecnica.Text = "........";
+            // 
+            // lblEconomica
+            // 
+            this.lblEconomica.AutoSize = true;
+            this.lblEconomica.Location = new System.Drawing.Point(178, 352);
+            this.lblEconomica.Name = "lblEconomica";
+            this.lblEconomica.Size = new System.Drawing.Size(34, 13);
+            this.lblEconomica.TabIndex = 19;
+            this.lblEconomica.Text = ".........";
+            // 
+            // lblOperacional
+            // 
+            this.lblOperacional.AutoSize = true;
+            this.lblOperacional.Location = new System.Drawing.Point(178, 391);
+            this.lblOperacional.Name = "lblOperacional";
+            this.lblOperacional.Size = new System.Drawing.Size(37, 13);
+            this.lblOperacional.TabIndex = 20;
+            this.lblOperacional.Text = "..........";
+            // 
+            // Factibilidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 511);
+            this.Controls.Add(this.lblOperacional);
+            this.Controls.Add(this.lblEconomica);
+            this.Controls.Add(this.lblTecnica);
+            this.Controls.Add(this.lblBeneficios);
+            this.Controls.Add(this.lblRecursos);
+            this.Controls.Add(this.lblDuracion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblAreaA);
+            this.Controls.Add(this.lblAreaS);
+            this.Controls.Add(this.lblProyecto);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Factibilidad";
+            this.Text = "Factibilidad";
+            this.Load += new System.EventHandler(this.Factibilidad_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblProyecto;
+        private System.Windows.Forms.Label lblAreaS;
+        private System.Windows.Forms.Label lblAreaA;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblDuracion;
+        private System.Windows.Forms.Label lblRecursos;
+        private System.Windows.Forms.Label lblBeneficios;
+        private System.Windows.Forms.Label lblTecnica;
+        private System.Windows.Forms.Label lblEconomica;
+        private System.Windows.Forms.Label lblOperacional;
+    }
+}
---
+++ ProyectoPlaneacion/ProyectoPlaneacion/Factibilidad.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPlaneacion
+{
+    public partial class Factibilidad : Form
+    {
+        public Factibilidad()
+        {
+            InitializeComponent();
+        }
+
+        private void Factibilidad_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CargarProyecto()
+        {
+            string ls = "";
+            DataRow dt = null;
+
+            ls = @"select
+                        f.id_proyecto
+                       ,p.denominacion
+                       ,a.descripcion
+                       ,p.duracion
+                       ,r.Recurso
+                       ,b.Beneficio
+                       from Factible f
+                       join Proyecto p
+                       on f.id_proyecto = p.id_proyecto
+                       join Area a
+                       on p.id_area  = a.id_area
+                       join Recurso r
+                       on p.id_proyecto = r.id_proyecto
+                       join Beneficio b
+                       on p.id_proyecto = b.id_proyecto";
+
+            if (auxiliar.c.SQLSelectRow(ls, ref dt))
+            {
+               //// lblProyecto.Text = dt[""].ToString();
+                //txtPoblacion.Text = dr["poblacion"].ToString();
+            }
+        }
+    }
+}
